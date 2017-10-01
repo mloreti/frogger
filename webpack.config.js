6c24be110a7b82271d0daba97eb176708fa0329e
Mickey Loreti <mloreti10@gmail.com>
2017-10-01 01:02:48 -0700

    Enable TsLint and EsLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    entry: "./src/index.ts",
-    module: {
-        loaders: [
-            { test: /\.ts$/,
-                exclude:path.resolve(__dirname, "node_modules"),
-                loader: "babel-loader!awesome-typescript-loader" }
-        ],
-    },
-    resolve: {
-		modules: [
-			__dirname,
-			path.resolve(__dirname, "src"),
-			"node_modules"
-		],
-        extensions: [ '.ts', '.js'],
-    },
-    output: {
-        path: __dirname + '/dist/',
-        filename: '[name].js',
-        publicPath : 'dist'
-    },
-    devServer: {
-        publicPath: "/",
-        contentBase: "./dist",
-        hot: true
-    },
-    devtool: "source-map"
+  entry: {
+    index: "./src/index.ts"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: "babel-loader!awesome-typescript-loader"
+      }
+    ]
+  },
+  resolve: {
+    modules: [__dirname, 
+      path.resolve(__dirname, "src"), 
+      "node_modules"
+    ],
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    path: __dirname + "/dist/",
+    filename: "[name].js",
+    publicPath: "dist"
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: "./dist",
+    hot: true
+  },
+  devtool: "source-map"
 };
